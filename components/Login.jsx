304eea3fa73388edd9773f9df058d8ca6337e345
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2022-04-17 15:00:18 +0530

    Need to finish styling on some other day

--- components/Login.jsx
+++ components/Login.jsx
@@ -5,31 +5,13 @@ export default function Home() {
   const { authenticate } = useMoralis();
 
   return (
-    <div className="bg-black relative">
-      <h1>Login screen</h1>
-      <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
-        <Image
-          className="object-cover rounded-full"
-          src="https://links.papareact.com/3pi"
-          alt="logo"
-          height={200}
-          width={200}
-        />
-        <button
-          onClick={authenticate}
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
-        >
-          Login to METAVERSE
-        </button>
-      </div>
-      <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          alt="background"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
+    <div className="bg-pink-900 h-screen flex items-center justify-center">
+      <button
+        className="px-8 py-4 bg-pink-500 hover:bg-pink-600 hover:shadow-lg hover:mb-1 transition-all rounded-lg h-fit"
+        onClick={authenticate}
+      >
+        Login to METAVERSE
+      </button>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,17 @@ export default function Home() {
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900">
+    <>
       <Head>
         <title>METAVERSE CHALLENGE</title>
         <meta name="description" content="Metaverse challenge for WEB3.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="max-w-screen-2xl mx-auto">
+      <div className="p-10 bg-pink-900">
         <Header />
         <Messages />
       </div>
-      {/* <button onClick={logout}>Logout</button> */}
-    </div>
+    </>
   );
 }
